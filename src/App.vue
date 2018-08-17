b6e85cd5be233269b4478b072d53a8a86c36068a
PrajaktaNatu <30647375+PrajaktaNatu@users.noreply.github.com>
2018-08-17 13:48:45 +0530

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,43 @@
 <template>
-    <div class="container">
-        <div>
-            <transition name="fade">
-                <router-view></router-view>
-            </transition>
-        </div>
-    </div>
+  <div id="app">
+      <div id="nav">
+          <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace>Logout</router-link>
+      </div>
+      <router-view @authenticated="setAuthenticated" />
+  </div>
 </template>
 
 <style>
-    .fade-enter-active, .fade-leave-active {
-      transition: opacity .5s
-    }
-    .fade-enter, .fade-leave-active {
-      opacity: 0
-    }
+
 </style>
 
 <script>
 
-    export default{
-    }
+export default{
+name: 'App',
+     data() {
+         return {
+             authenticated: false,
+             mockAccount: {
+                 username: "nraboy",
+                 password: "password"
+             }
+         }
+     },
+     mounted() {
+         if(!this.authenticated) {
+             this.$router.replace({ name: "login" });
+         }
+     },
+     methods: {
+         setAuthenticated(status) {
+             this.authenticated = status;
+         },
+         logout() {
+             this.authenticated = false;
+         }
+     }
+ }
+
+
 </script>
